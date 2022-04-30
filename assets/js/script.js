bc1d88a5bc3f4fb96c76974eb7a8514a71b7b400
Shahidul Islam <shahid.okay@gmail.com>
2022-04-30 14:29:10 +0000

    delete commented code in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,10 +21,6 @@ function showfullName() {
 }
 
 // second page
-// submit.addEventListener('click', getNumber);
-
-// let randomNum = Math.floor(Math.random() * 20);
-
 let count = 0;
 let negCount = 5;
 
@@ -43,7 +39,6 @@ numbers.forEach(number => {
         
         let name = document.getElementById('name').value;
 
-        // let x = parseFloat(document.getElementById('numInput'))
         // console.log(x);
         if (numInput === randomNum){
             document.getElementById('win').innerHTML = `congratulations! ${name}</br> you won the game.`;
@@ -64,7 +59,6 @@ numbers.forEach(number => {
         }else {
             document.getElementById('hint').innerHTML = `getting there ${name}, go down a bit more!!`;
             document.getElementById('hint').style. color = 'brown'
-
         }
 
         count ++;
@@ -79,68 +73,10 @@ numbers.forEach(number => {
             document.getElementById('hide').remove();
             // hide this div
             document.getElementById('hint').remove();
-            
-
-    
         }
-        
+  
     })
-    
-    
 })
-
-
-// function getNumber() {
-//     let input = Number(document.getElementById('user-input').value);
-
-//     // cursor stays on the input value
-//     document.getElementById('user-input').focus();
-
-//     // always give empty string on the user input
-//     document.getElementById('user-input').value = '';
-
-//     let submit = document.getElementById('submit')
-
-//     let name = document.getElementById('name').value;
-
-//     if(input === randomNum) {
-//         submit.remove();
-//         document.getElementById('win').innerHTML = `congratulations! ${name}</br> you won the game.`;
-//         // hide this div if game wins
-//         document.getElementById('hide').style.display = 'none';
-//         // hide the timer 
-//         document.getElementById('timer').style.display = 'none';
-
-//     }else if(input < randomNum) {
-//         document.getElementById('hint').innerHTML = `almost there ${name}, go up a bit more!! `;
-//         document.getElementById('hint').style. color = 'green'
-//     } else {
-//         document.getElementById('hint').innerHTML = `getting there ${name}, go down a bit more!!`;
-//         document.getElementById('hint').style. color = 'brown'
-
-//     }
-//     count ++;
-//     negCount --;
-//     document.getElementById('submitted').innerHTML = ` ${count} times`;
-//     document.getElementById('chance-left').innerHTML = ` ${negCount} times`;
-
-//     if (count === 5) {
-//         submit.remove();
-//         document.getElementById('win').innerHTML = `oppss... ${name}</br> you lost the game. </br>The secret number was ${randomNum}`;
-//         // hide this div if game lost
-//         document.getElementById('hide').style.display = 'none';
-//         // hide the timer 
-//         document.getElementById('timer').style.display = 'none';
-
-//     }
-// }
-
-// enter key function to user-input and submit button
-// document.getElementById('user-input').addEventListener('keydown', function(event) {
-//     if(event.key === 'Enter') {
-//         getNumber();
-//     }
-// })
 
 // enter key function to main page user name and lets play button
 document.getElementById('name').addEventListener('keydown', function(event) {
@@ -162,16 +98,17 @@ function restartGame() {
 
 // timer
 let timeleft = 29;
+
 function timer(){
     let name = document.getElementById('name').value;
     let getTimer = setInterval(function(){
     if(timeleft === 0){
         clearInterval(getTimer);
         document.getElementById("timer").innerHTML = "Time's Up";
-        // submit.remove();
         document.getElementById('numberSubmit').style.display = 'none';
-
         document.getElementById('win').innerHTML = `Uh-Ahh!!! ${name} </br> please try again`;
+
+        // hide these divs
         document.getElementById('hide').style.display = 'none';
         document.getElementById('hint').remove();
 
@@ -194,16 +131,4 @@ document.getElementById('name').addEventListener('keyup', function(event) {
     }
 })
 
-// disable submit button if nothing is entered
-// document.getElementById('submit').disabled = true;
 
-// listen for key up to enable button
-
-// document.getElementById('user-input').addEventListener('keyup', function(event){
-//     if (event.target.value === 0){
-//     document.getElementById('submit').disabled = true;
-//     } else {
-//     document.getElementById('submit').disabled = false;
-
-//     }
-// })
